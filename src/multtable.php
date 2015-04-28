888960f3ff1d6d7d7ee12b1b268344aaa4ca87a5
Brandon Swanson Ubuntu <swanyriver@gmail.com>
2015-04-28 15:28:04 -0700

    multiplication table working 30minutes

--- src/multtable.php
+++ src/multtable.php
@@ -46,7 +46,30 @@ if($incorect_param){
     return;
 }
 
-echo "all tests passed";
+//echo "all tests passed";
+
+
+//create table
+echo "<style>
+th {font-weight: bold;}
+table * {padding:5px;}
+</style>
+<table border=1> 
+<tr>
+<td>";
+
+for ($i = $min_mp; $i <= $max_mp; $i++){
+    echo "<th>$i";
+}
+
+for ($row = $min_mc; $row <= $max_mc; $row++){
+    echo "<tr><th>$row";
+    for ($col = $min_mp; $col <= $max_mp; $col++){
+        echo "<td>" . ($row * $col);
+    }
+}
+
+echo "</table>";
 
 
 ?>
