02c9a6b69f1e88af245ee0c462d0b7c7bf68d791
Brandon Swanson <swanyriver@gmail.com>
2015-04-27 22:07:42 -0700

    checking multtable paramaters

---
+++ src/multtable.php
@@ -0,0 +1,47 @@
+<?php
+
+$getKeys = new $array();
+$getKeys[] = 'min-multiplicand';
+$getKeys[] = 'max-multiplicand';
+$getKeys[] = 'min-multiplier';
+$getKeys[] = 'max-multiplier';
+
+//check that all four get paramaters are provided and integers
+$missingParam = 0;
+$badParam = 0;
+foreach ($getKeys as $value) {
+    if (! isset($_GET[$value])){
+        echo "Missing parameter $value.<br>";
+        $missingParam = 1;
+    }
+    if(! is_int($_GET[$value])){
+        echo "$value must be an integer.<br>";
+        $badParam = 1;
+    }
+}
+if ($missingParam || $badParam){
+    return;
+}
+
+//capture get paramaters
+$min_mc = intval($_GET['min-multiplicand']);
+$max_mc = intval($_GET['max-multiplicand']);
+$min_mp = intval($_GET['min-multiplier']);
+$max_mp = intval($_GET['max-multiplier']);
+
+
+//error check get paramaters
+if($min_mc>$max_mc){
+    echo "Minimum multiplicand larger than maximum.<br>"
+    return
+}
+
+if($min_mp>$max_mp){
+    echo "Minimum multiplier larger than maximum.<br>"
+    return
+}
+
+echo "all tests passed"
+
+
+?>
